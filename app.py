300fbe0aee1a2c63693b455ebe958bf30314bb10
Mayk Brenndon <maykbrendon.antunes@gmail.com>
2023-01-18 12:46:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from threading import Thread
 def send_message_telegram():
     while True:
         try:
-            criptomoeda = yf.Ticker('MATIC').history(period='1d')
+            criptomoeda = yf.Ticker('MATIC-USD').history(period='1d')
             criptomoeda['Close'] = criptomoeda['Close'].map(lambda x: x*dolar)
             last_value = criptomoeda['Close'][0]
-            message = f"Ultimo fechamento da {opt_cripto}: R$ {last_value:.2f}".replace('.',',')
+            message = f"Ultimo fechamento da MATIC: R$ {last_value:.2f}".replace('.',',')
             send_message(message=message)
             sleep(3.600)
         except:
